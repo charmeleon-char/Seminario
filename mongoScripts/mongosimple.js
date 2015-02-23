8224e65804f371a0e958a69ce87db131d9f62db7
Ramon <ramoncalixb@gmail.com>
2015-02-22 20:17:17 -0600

    Ejercicio de mongodb para subir 10 registros

---
+++ mongoScripts/mongosimple.js
@@ -0,0 +1,44 @@
+// mongo < scriptName.js
+
+use sw201501;
+show collections;
+//var doc = {"otherAttri":"Some other value"};
+//db.testSimple.insert(doc);
+//db.testSimple.findOne();
+
+//Revisando Esquema de Base de Datos
+checkSchema = function(){
+  var collUsuariosExists = db.usuarios.count();
+  var collAulasExists = db.aulas.count();
+  if(!collUsuariosExists){
+    var firstUser = {"username":"administrator",
+                     "pswd":"whenthecatsgooutthemicepartyallnight",
+                     "name":"Administrador del Sitio",
+                     "fchIng": new Date(),
+                     "roles":["admin","all"]
+                   },
+        secondUser = {"username":"guest",
+                         "pswd":"bibidibabidiboo",
+                         "name":"Guest User",
+                         "fchIng": new Date(),
+                         "roles":["all"]
+                       };
+        db.usuarios.insert(firstUser);
+        db.usuarios.insert(secondUser);
+  }
+  if(!collAulasExists){
+    // implementar con 10 aulas.
+    // del Edificio F
+    for(var i =1 ;i<11;i++)
+    {
+      var aula = {"NumeroAula": i,"Edificio":"F","fchIng":new Date(), "pasillo":i+"F" 
+                   };
+     db.aulas.insert(aula);              
+    }
+  }
+}
+
+
+checkSchema();
+db.usuarios.find().pretty();
+db.aulas.find().pretty();
